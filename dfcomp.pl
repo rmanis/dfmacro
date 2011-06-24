0321d13c2705084cb2411dd031d6d0e27c6bfa86
Richie Manis <richie.manis@gmail.com>
2011-06-23 23:47:30 -0400

    Separated "keybindings" from main script

--- dfcomp.pl
+++ dfcomp.pl
@@ -5,30 +5,10 @@ use Parse::RecDescent;
 # $::RD_HINT = "yo";
 #$::RD_TRACE = "yo";
 
-%commands = (s7 => "CURSOR_UPLEFT_FAST",
-             s9 => "CURSOR_UPRIGHT_FAST",
-             s1 => "CURSOR_DOWNLEFT_FAST",
-             s3 => "CURSOR_DOWNRIGHT_FAST",
-
-             s8 => "CURSOR_UP_FAST",
-             s2 => "CURSOR_DOWN_FAST",
-             s4 => "CURSOR_LEFT_FAST",
-             s6 => "CURSOR_RIGHT_FAST",
-
-             8 => "CURSOR_UP",
-             2 => "CURSOR_DOWN",
-             4 => "CURSOR_LEFT",
-             6 => "CURSOR_RIGHT",
-
-             3 => "CURSOR_DOWNRIGHT",
-             7 => "CURSOR_UPLEFT",
-             9 => "CURSOR_UPRIGHT",
-             1 => "CURSOR_DOWNLEFT",
-
-             "." => "SELECT",
-             i => "DESIGNATE_STAIR_UPDOWN",
-             d => "DESIGNATE_DIG",
-             x => "DESIGNATE_UNDO");
+open CONF, "keys.pl";
+@conf = <CONF>;
+eval join "", @conf;
+close CONF;
 
 $grammar = q(
 	{ my %functions; }
---
+++ keys.pl
@@ -0,0 +1,26 @@
+
+%commands = (s7 => "CURSOR_UPLEFT_FAST",
+             s9 => "CURSOR_UPRIGHT_FAST",
+             s1 => "CURSOR_DOWNLEFT_FAST",
+             s3 => "CURSOR_DOWNRIGHT_FAST",
+
+             s8 => "CURSOR_UP_FAST",
+             s2 => "CURSOR_DOWN_FAST",
+             s4 => "CURSOR_LEFT_FAST",
+             s6 => "CURSOR_RIGHT_FAST",
+
+             8 => "CURSOR_UP",
+             2 => "CURSOR_DOWN",
+             4 => "CURSOR_LEFT",
+             6 => "CURSOR_RIGHT",
+
+             3 => "CURSOR_DOWNRIGHT",
+             7 => "CURSOR_UPLEFT",
+             9 => "CURSOR_UPRIGHT",
+             1 => "CURSOR_DOWNLEFT",
+
+             "." => "SELECT",
+             i => "DESIGNATE_STAIR_UPDOWN",
+             d => "DESIGNATE_DIG",
+             x => "DESIGNATE_UNDO");
+
